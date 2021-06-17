ffe9e8d8548c278a7f151f5d70756bfca4ea40c0
Luciano Lococo <lucianolococo@hotmail.com>
2021-06-17 20:25:06 -0300

    MÓDULO ESCANEO

---
+++ Negocio/N_Apunte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Negocio
+{
+    //AGREGADO LUCHO
+    public class N_Apunte
+    {
+        public int? id_Apuntes { get; set; }
+        public string tituloApunte { get; set; }
+        public byte id_estado { get; set; }
+        public float Stock { get; set; }
+        public int cantidadHojas { get; set; }
+        public byte estado { get; set; }
+
+        public int InsertarApunte()
+        {
+            return Datos.Apunte.InsertarApunte(tituloApunte, id_estado, Stock, cantidadHojas, estado);
+        }
+    }
+}
--- Presentacion/Controllers/EscaneoController.cs
+++ Presentacion/Controllers/EscaneoController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Negocio;
 
 namespace Presentacion.Controllers
 {
     public class EscaneoController : Controller
     {
-        // GET: Escaneo
+        //AGREGADO LUCHO
         public ActionResult Index()
         {
-            return View();
+            List<N_ProfesorApunte> listaprofesorapunte = Negocio.N_ProfesorApunte.ListardeNoactualizados();
+            return View(listaprofesorapunte);
         }
+        public ActionResult AgregarApunte(int? id)
+        {
+            Session["id"] = id;
+            N_Apunte apunte = new N_Apunte();
+            return View(apunte);
+        }
+        public ActionResult InsertarApunte(N_Apunte apunte)
+        {
+            apunte.estado = 1;
+            apunte.id_estado = 1;
+            apunte.InsertarApunte();
+            return RedirectToAction("Digitalizar");
+        }
+        public ActionResult Digitalizar(int? id)
+        {
+            id = (int?)Session["id"];
+            N_ProfesorApunte profesorapunte = N_ProfesorApunte.ListardeNoactualizados().Where(x => x.IdProfesorApunte == id).FirstOrDefault();
+            profesorapunte.Digitalizar(profesorapunte.IdProfesorApunte.Value);
+            return View("Index", N_ProfesorApunte.ListardeNoactualizados());
+        }
+
+
     }
 }
