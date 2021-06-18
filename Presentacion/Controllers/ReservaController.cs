e5358dc20ea691b391dd35f40a8a412df238fbf7
emmanuelhaedo.eh <emmanuelhaedo.eh@gmail.com>
2021-06-18 16:26:07 -0300

    Correcciones Ema

---
+++ Presentacion/Controllers/ReservaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentacion.Controllers
+{
+    public class ReservaController : Controller
+    {
+        // GET: Reserva
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
