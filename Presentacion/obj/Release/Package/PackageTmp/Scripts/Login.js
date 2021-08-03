07fea85179f479189787719228048c9fb3e2a5f3
emmanuelhaedo.eh <emmanuelhaedo.eh@gmail.com>
2021-08-03 09:14:42 -0300

    03/08/2021

---
+++ Presentacion/obj/Release/Package/PackageTmp/Scripts/Login.js
@@ -0,0 +1,24 @@
+﻿function ValidarUsuario()
+{
+    debugger
+
+    $.ajax({
+        url: '/Home/Inicio',
+        type: 'POST',
+        async: false,
+        dataType: 'text',
+        processData: false,
+        data: $('#FrmLogin').serialize(),
+        success: function (data) {
+            var resultado = $.parseJSON(data);
+            if (resultado.Mensaje !== '') {
+                $('#msgErrorLogin').show('slow');
+                $('#msgErrorLoginDes').append(resultado.Mensaje);
+            }
+            else {
+                window.location = "/Usuario/Listar";
+            }
+
+        }
+    });
+}
